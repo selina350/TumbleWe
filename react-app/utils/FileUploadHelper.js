8fe4b3297347ec7053a58ad2c43a359dd99fc17a
Selina Shi <selina@Selinas-MacBook-Air.local>
2023-11-28 22:00:47 -0600

    fix subdomain and added content-type during loading

--- react-app/utils/FileUploadHelper.js
+++ react-app/utils/FileUploadHelper.js
@@ -1,4 +1,5 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import mime from "mime";
 
 const client = new S3Client({
   region: "us-west-2",
@@ -9,10 +10,14 @@ const client = new S3Client({
 });
 
 export const uploadFileToS3 = async (file, appId) => {
+  // Determine the content type based on file extension
+  const ContentType = mime.getType(file.name) || "application/octet-stream";
+
   const command = new PutObjectCommand({
     Bucket: process.env.S3_BUCKET,
     Key: `${appId}/${file.name}`,
     Body: file,
+    ContentType,
   });
 
   try {
